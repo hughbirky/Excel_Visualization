3354bbd7c9fbf76108651bf84e06ec7bb9f3531e
Hugh Birky <hughmbirky@gmail.com>
2024-03-13 12:10:05 -0500

    Conditional Tabs

--- server.R
+++ server.R
@@ -19,6 +19,66 @@ shinyServer(function(input, output, session) {
     
   })
 
+  
+  
+  # Function for generating the plot
+  generatePlot <- function(data_filtered, input) {
+    # Filters the data fram to only have the columns we want from the select input
+    data_filtered <- data1$data %>%
+      select(input$x_column,input$y_column)
+    # Getting rid of NA values
+    data_filtered <-na.omit(data_filtered)
+    
+    # Plotting Data
+    plot <- ggplot(data_filtered, aes_string(x = input$x_column, y = input$y_column)) +
+      geom_point(size = input$point_size, color = input$point_color) +
+      labs(x = input$x_column, y = input$y_column, title = "Scatterplot") +
+      theme(
+        legend.position = "none",
+        plot.title = element_text(size=20),
+        axis.title.x = element_text(angle = 0, hjust = 0.5,size = 30),
+        axis.title.y = element_text(angle = 90, vjust = 0.5,size = 30),
+        axis.text.x = element_text(size = 25),  # Increase size of x-axis numbers
+        axis.text.y = element_text(size = 25),  # Increase size of y-axis numbers
+        text = element_text(family = "Arial")
+      )
+    
+    
+    # Plotting in case there are gridlines
+    if (input$gridlines && input$minor_gridlines) {
+       plot +
+        theme(plot.background = element_rect(fill = input$background_color),
+              panel.background = element_rect(fill = input$background_color),
+              panel.grid.major = element_line(color = input$major_gridline_color),
+              panel.grid.minor = element_line(color = input$minor_gridline_color)
+              )
+      # Condition where you only want minor gridlines
+    } else if(!input$gridlines && input$minor_gridlines) {
+      plot +
+        labs(x = input$x_column, y = input$y_column, title = "Scatterplot") +
+        theme(plot.background = element_rect(fill = input$background_color),
+              panel.background = element_rect(fill = input$background_color),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_line(color = input$minor_gridline_color)
+              )
+      # Condition where you only want major gridlines
+    } else if(input$gridlines && !input$minor_gridlines) {
+      plot +
+        theme(plot.background = element_rect(fill = input$background_color),
+              panel.background = element_rect(fill = input$background_color),
+              panel.grid.major = element_line(color = input$major_gridline_color),
+              panel.grid.minor = element_blank())
+      # Condition where you want neither
+    } else if(!input$gridlines && !input$minor_gridlines) {
+      plot +
+        theme(plot.background = element_rect(fill = input$background_color),
+              panel.background = element_rect(fill = input$background_color),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank())
+    }
+  }
+
+  
   # This actually outputs the plot
   output$plot <- renderPlot({
     
@@ -27,17 +87,30 @@ shinyServer(function(input, output, session) {
     req(input$y_column)
     req(input$x_column)
     
-    # Filters the data fram to only have the columns we want from the select input
-    data_filtered <- data1$data %>%
-      select(input$x_column,input$y_column)
-    # Getting rid of NA values
-    data_filtered <-na.omit(data_filtered)
+    # Calling the function to generate the plot
+    generatePlot(data_filtered, input)
     
-    # Plots the data on a scatterplot
-    ggplot(data_filtered, aes_string(x = input$x_column,y = input$y_column)) +
-      geom_point(size = input$point_size, color = input$point_color) +
-      labs(x = input$x_column, y = input$y_column, title = "Scatterplot")
     
   })
+  
+  
+  
+  # Downloading the plot
+  output$save_graph <- downloadHandler(
+    # Generating generic file name for the initial spot
+    filename = function(){
+      paste("plot", Sys.Date(), ".jpg", sep = "")
+    },
+
+    # Generating the content needed to save the file
+    content = function(file) {
+      req(data1$data)
+      req(input$y_column)
+      req(input$x_column)
+      
+      plot <- generatePlot(data_filtered, input)
+      ggsave(file, plot,scale = 1, width = input$plot_width,height = input$plot_height)
+    }
+  )
   
 })
--- ui.R
+++ ui.R
@@ -6,25 +6,79 @@ shinyUI(fluidPage(
   # Sidebar with a file input and select input
   sidebarLayout(
     sidebarPanel(
-      # Input: Select a file
-      fileInput("file1", h3("Choose xlsx File"), accept = c(".xlsx")),
       
-      # # Selecting which item you want
-      # selectInput("plotType", label = h3("Graph Type"),
-      #             choices = list("Scatterplot" = 1, "Box and Whisker" = 2)),
-      
-      # Selecting which item you want for each column
-      selectInput("x_column", label = h3("X Data"),
-                  choices = list("")),
-      selectInput("y_column", label = h3("Y Data"),
-                  choices = list("")),
-      
-      # Select inputs for the points on the plot
-      # Select point size
-      sliderInput("point_size", "Point Size", value = 1, min = 0.01, max = 5),
-      
-      # Select point color
-      colourInput("point_color", "Point Color", "blue")
+      # Tabset panel for distinguishing between different sections
+      tabsetPanel(
+        
+        # Tab1 : Plot Settings
+        tabPanel("Data Import",
+                 # Input: Select a file
+                 fileInput("file1", "Choose xlsx File", accept = c(".xlsx")),
+                  
+                 # Selecting which graph type you want
+                 selectInput("plotType", label = "Graph Type",
+                              choices = c("Scatterplot","Boxplot")),
+                 
+                # Panel for Scatterplot Settings
+                 conditionalPanel(
+                   condition = "input.plotType == 'Scatterplot'",
+                   # Selecting which item you want for each column
+                   selectInput("x_column", label = "X Data",
+                               choices = list("")),
+                   selectInput("y_column", label = "Y Data",
+                               choices = list(""))
+                 ), 
+                  
+                 
+                 
+                  
+                  
+        ),
+        
+        # Tab 2: Exporting Data
+        tabPanel("Plot Settings",
+                 # Select inputs for the points on the plot
+                 # Select point size
+                 sliderInput("point_size", "Point Size", value = 1, min = 0.01, max = 5),
+                 # Select point color
+                 colourInput("point_color", "Point Color", "black"),
+                 colourInput("background_color", "Background Color", "lightgrey"),
+                 colourInput("panel_color", "Panel Color", "grey"),
+
+                 
+                 # Panel for Scatterplot Settings
+                 conditionalPanel(
+                   condition = "input.plotType == 'Scatterplot'",
+                   
+                   # Checkbox for gridlines
+                   checkboxInput("gridlines", "Show Major Gridlines", value = TRUE),
+                   checkboxInput("minor_gridlines", "Show Minor Gridlines", value = TRUE),
+                   
+                   # Color picker for gridline color
+                   # Conditional panel for showing the color option for the gridlines
+                   conditionalPanel(
+                      condition = "input.gridlines",
+                      colourInput("major_gridline_color", "Major Gridline Color", value = "white"),
+                   ),
+                   conditionalPanel(
+                     condition = "input.minor_gridlines",
+                     colourInput("minor_gridline_color", "Minor Gridline Color", value = "white"))
+                 )
+          
+        ),
+        
+        tabPanel("Export",
+                 # Input for plot width
+                 numericInput("plot_width", "Plot Export Width", value = 8),
+                 
+                 # Input for plot height
+                 numericInput("plot_height", "Plot Export Height", value = 6),
+                 
+                 downloadButton("save_graph","Save Graph")
+          
+        )
+        
+      )
     ),
     
     # Main panel for displaying outputs
